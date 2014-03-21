3dd5e65f0e9039fbe8ee33cbe859f6fc56accb45
marcoscirea <marco.prolog@gmail.com>
2014-03-21 09:34:33 +0100

    beautified mood selector

--- Assets/Scripts/MoodTrigger.cs
+++ Assets/Scripts/MoodTrigger.cs
@@ -4,13 +4,24 @@ using System.Collections;
 public class MoodTrigger : MonoBehaviour {
 
     MoodSelector m;
+    Animator anim;
 
 	// Use this for initialization
 	void Start () {
         m = GameObject.Find("MoodSelector").GetComponent<MoodSelector>();
+        anim = gameObject.GetComponent<Animator>();
 	}
 
     void OnMouseUpAsButton(){
+        anim.SetTrigger("Click");
         m.SetMood(gameObject.name);
+    }
+
+    void OnMouseEnter() {
+        anim.SetTrigger("OnMouse");
+    }
+
+    void OnMouseExit() {
+        anim.SetTrigger("NoMouse");
     }
 }
